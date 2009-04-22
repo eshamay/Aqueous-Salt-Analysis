308a9c6238ec2d7be156ce7c5ecd383196a62187
Eric Shamay <eric@guru.uoregon.edu>
2009-04-22 16:22:36 -0700

    reworking the dir

--- Analysis/scripts/OrderParameters.py
+++ Analysis/scripts/OrderParameters.py
@@ -1,4 +1,5 @@
 #!/Library/Frameworks/Python.framework/Versions/Current/bin/python
+import sys
 from scipy import *
 import scipy.io.array_import
 from scipy.optimize import leastsq
@@ -11,23 +12,36 @@ from DensityProfiler import DensityProfiler
 class OrderParameters:
 	
 	def __init__(self, system):
+
+		
 		self.data = self.DataDict(system+'.orderparams.avg.dat')
 		density = DensityProfiler(['../Densities/'+system+'.density.avg.dat'])
 		self.dens = density.data[0]
 		self.fits = density.fits[0]
-		
+		self.names = {
+					  'x':0,
+					  'phi':1, 'theta':2, 'psi':3,
+					  'sinphi':4, 'sintheta':5, 'sinpsi':6,
+					  'sin2phi':7, 'sin2theta':8, 'sin2psi':9,
+					  'sinsqphi':10, 'sinsqtheta':11, 'sinsqpsi':12,
+					  'cosphi':13, 'costheta':14, 'cospsi':15,
+					  'cos2phi':16, 'cos2theta':17, 'cos2psi':18,
+					  'cossqphi':19, 'cossqtheta':20, 'cossqpsi':21,
+					  'sinthetacos2phi':22, 'sinthetacos2psi':23,
+					  'sinthetasqcos2phi':24, 'sinthetasqcos2psi':25,
+					  'n':26
+		}
+
+
+
 	def DataDict(self, filename):
 
-		data = {}
+		data = []
 
 		file = scipy.io.array_import.read_array(filename)
-		x = file[:,0]
-		S1 = file[:,1]
-		S2 = file[:,2]
 
-		data['x'] = x
-		data['S1'] = S1
-		data['S2'] = S2
+		for i in range(len(file[0])):
+			data.append(file[:,i])
 
 		return data
 
@@ -47,34 +61,16 @@ class OrderParameters:
 		xmin = -10
 		xmax = 10
 		# plot the S1 curve
-		S1ax.plot(d['x'],d['S1'],'k', linewidth=3)
+		x = d[self.names['x']]
+		s1 = d[self.names['cossqtheta']]
+		for i in range(len(s1)):
+			s1[i] = 0.5*(3.0*s1[i]-1.0)
+		S1ax.plot(d[self.names['x']],s1,'k', linewidth=3, label='s1')
 		S1ax.set_ylim(-0.2,0.1)
 		# plot the S2 curve
-		S2ax.plot(d['x'],d['S2'],'k', linewidth=3)
-		S2ax.set_ylim(-0.4,0.25)
+		#S2ax.plot(d['x'],d['S2'],'k', linewidth=3)
+		#S2ax.set_ylim(-0.4,0.25)
 
-		'''
-		# find the S1 minimum
-		d['x'] = list(d['x'])
-		d['S1'] = list(d['S1'])
-		S1range = d['S1'][d['x'].index(-5.0):d['x'].index(5.0)] 
-		S1min_index = d['S1'].index( min(S1range) )
-		#plot the S1 min marker
-		S1ax.axvline(d['x'][S1min_index], color='k', linestyle='dashed', linewidth=2)
-		'''
-		'''
-		#find S2 max
-		d['S2'] = list(d['S2'])
-		S2range = d['S2'][d['x'].index(-5.0):d['x'].index(5.0)] 
-		S2max_index = d['S2'].index( max(S2range) )
-		# plot the S2 maximum line-marker
-		S2ax.axvline(d['x'][S2max_index], color='k', linestyle='dashed', linewidth=2)
-		'''
-
-		#dens.plot(self.dens['position'], self.dens['h2o'], 'k-', linewidth=3)
-		#dens.plot(self.dens['position'], self.dens['anion'], 'r-', linewidth=3)
-		#dens.plot(self.dens['position'], self.dens['cation'], 'b-', linewidth=3)
-		
 		# shows the interface and plots vertical reference lines where the ions peak
 		for ax in fig.get_axes():
 			ax.axvspan(-self.fits['p_h2o'][3]*2.197/2, self.fits['p_h2o'][3]*2.197/2, facecolor='g', alpha=0.2)
